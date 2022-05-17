81962ba7eae5d05164169d24517a2f7924c67a2d
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-17 12:52:08 +0300

    refactor:change views.py file to show list of subscribers

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,20 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,SelectField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileField,FileAllowed
+from wtforms import StringField,TextAreaField, SubmitField,ValidationError
+from wtforms.validators import DataRequired,Email
+from flask_login import current_user
+from ..models import User
 
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
-#     submit = SubmitField('Save')
 
-class BlogForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    category = StringField('Category',validators=[DataRequired()])
-    blog = TextAreaField('Your blog', validators=[DataRequired()])
+class CreateBlog(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    content = TextAreaField('Blog Content',validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class UpdateProfile(FlaskForm):
+    username = StringField('Enter Your Username', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(),Email()])
+    bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
+    profile_picture = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError("The Email has already been taken!")
     
-    submit = SubmitField('Blog')
-
-class CommentForm(FlaskForm):
-    comment = TextAreaField('Leave a comment',validators=[DataRequired()])
-    submit = SubmitField('Comment')
-
-
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("The username has already been taken")
--- app/main/views.py
+++ app/main/views.py
@@ -1,68 +1,58 @@
-
-from flask import render_template,request,redirect,url_for,request,flash,abort
-
-from app.models import User,Blog, Comment,Subscriber
-from . import main
-# from ..requests import get_quotes
-from flask_login import current_user, login_required
-from ..models import User,Blog,Comment,Subscriber
-from .forms import BlogForm,CommentForm
+from flask import render_template,redirect,url_for,abort,request,flash
+from app.main import main
+from app.models import User,Blog,Comment,Subscriber
+from .forms import UpdateProfile,CreateBlog
+from .. import db
+from app.requests import get_quotes
 from flask_login import login_required,current_user
 from ..email import mail_message
-
 import secrets
 import os
+
+
 @main.route('/')
 def index():
-  
-    
-    blogs = Blog.query.order_by(Blog.date_created.desc()).all()    
-    return render_template("index.html",blog = blogs)
+    quotes = get_quotes()
+    blogs = Blog.query.order_by(Blog.posted.desc()).all() 
+    return render_template('index.html',quote = quotes, blogs=blogs)
 
-@main.route('/create_blog', methods = ['POST','GET'])
-
+@main.route('/new_post', methods=['POST','GET'])
+@login_required
 def new_blog():
-    subscribers = Subscriber.query.all()
-    form = BlogForm()
+    # subscribers = Subscriber.query.all()
+    form = CreateBlog()
     if form.validate_on_submit():
         title = form.title.data
-        blog = form.blog.data
-        category = form.category.data
-        user_id = current_user
-        new_blog_object = Blog(blog=blog,user_id=current_user._get_current_object().id,category=category,title=title)
-        new_blog_object.save_b()
-        for subscriber in subscribers:
-            mail_message("New Blog Post","email/new_blog",subscriber.email,blog=blog)
-        return redirect(url_for('main.index'))
-        
-    return render_template('create_blog.html', form = form)
-
-@main.route('/comment/<int:blog_id>', methods = ['POST','GET'])
-
-def comment(blog_id):
-
-    form = CommentForm()
-    blog = Blog.query.get(blog_id)
-    all_comments = Comment.query.filter_by(blog_id = blog_id).all()
-    if form.validate_on_submit():
-        comment = form.comment.data 
-        blog_id = blog_id
-        user_id = current_user
-        new_comment = Comment(comment = comment,user_id = user_id,blog_id = blog_id)
+        content = form.content.data
+        user_id =  current_user
+        blog = Blog(title=title,content=content,user_id=current_user._get_current_object().id)
  
+        blog.save()
+        # for subscriber in subscribers:
+        #     mail_message("New Blog Post","email/new_blog",subscriber.email,blog=blog)
+        return redirect(url_for('main.index'))
+        # flash('You Posted a new Blog')
+    return render_template('newblog.html', form = form)
+@main.route('/blog/<id>')
+def blog(id):
+    comments = Comment.query.filter_by(blog_id=id).all()
+    blog = Blog.query.get(id)
+    return render_template('blog.html',blog=blog,comments=comments)
+    
 
-        title = f'{blog.title} comment'
-        return redirect(url_for('main.index', blog_id = blog_id))
-    return render_template('comment.html', form =form, blog = blog,all_comments=all_comments)
 
-@main.route('/subscribe',methods = ['POST','GET'])
-def subscribe():
-    email = request.form.get('subscriber')
-    new_subscriber = Subscriber(email = email)
-    new_subscriber.save_subscriber()
-    mail_message("Subscribed to Norah's Blog","email/welcome_subscriber",new_subscriber.email,new_subscriber=new_subscriber)
-    flash('Sucessfuly subscribed')
-    return redirect(url_for('main.index'))
+
+
+@main.route('/comment/<blog_id>', methods = ['Post','GET'])
+@login_required
+def comment(blog_id):
+    blog = Blog.query.get(blog_id)
+    comment =request.form.get('newcomment')
+    new_comment = Comment(comment = comment, user_id = current_user._get_current_object().id, blog_id=blog_id)
+    new_comment.save()
+    return redirect(url_for('main.blog',id = blog.id))
+
+
 
 @main.route('/blog/<blog_id>/delete', methods = ['POST'])
 @login_required
@@ -73,3 +63,12 @@ def delete_post(blog_id):
     blog.delete()
     flash("You have deleted your Blog succesfully!")
     return redirect(url_for('main.index'))
+
+
+@main.route('/user/<string:username>')
+def user_posts(username):
+    user = User.query.filter_by(username=username).first()
+    page = request.args.get('page',1, type = int )
+    blogs = Blog.query.filter_by(user=user).order_by(Blog.posted.desc()).paginate(page = page, per_page = 4)
+    return render_template('userposts.html',blogs=blogs,user = user)
+
