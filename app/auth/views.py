9f9bfc8bbdff254cdc57965389b45377876c2295
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-17 12:51:32 +0300

    feat:add subscribe route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,49 +1,52 @@
-
-from . import auth
-from flask import render_template,redirect,url_for
-from ..models import User
-from .forms import LoginForm, RegistrationForm
-from .. import db
-from flask import render_template,redirect,url_for, flash,request
-from flask_login import login_user
-from ..models import User
-from .forms import LoginForm,RegistrationForm
+from flask import render_template,flash, request, redirect, url_for
+from flask_login import login_user, logout_user,login_required
+from app.auth import auth
+from app.models import Subscriber, User
+from .forms import RegistrationForm,LoginForm,SubscribeForm
 from ..email import mail_message
-from flask_login import login_user,logout_user,login_required
 
-
-@auth.route('/register',methods = ["GET","POST"])
-def register():
+@auth.route('/signup',methods = ["POST","GET"])
+def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
+        user = User(username=form.username.data, email = form.email.data, password=form.password.data)
+        user.save()
+        mail_message("Welcome to Norah's-Blog","email/welcome",user.email,user=user)
+        return  redirect(url_for('auth.login'))
+    return render_template('auth/signup.html',registration_form=form )
 
-        # mail_message("Welcome to bloges","email/welcome_user",user.email,user=user)
 
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login',methods = ['POST','GET'])
 def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(username = login_form.username.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user != None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Wrong Username or Password')
+    return render_template('auth/login.html',form = form)
 
-        flash('Invalid username or Password')
-
-    title = " login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+@auth.route('/subscribe',methods = ['POST','GET'])
+def subscribe():
+    form = SubscribeForm()
+    if form.validate_on_submit():
+        email = request.form.get('subscriber')
+        new_subscriber = Subscriber(email = email)
+        new_subscriber.save_subscriber()
+        mail_message("Subscribed to Norah's Blog","email/welcome_subscriber",new_subscriber.email,new_subscriber=new_subscriber)
+        flash('Sucessfuly subscribed')
+        
+         
+        return redirect(url_for('main.index'))   
+        
+    return render_template('auth/subscribe.html',form = form)
 
     
-#....
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+    
