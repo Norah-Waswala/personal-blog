bcfb0cdc921bdbf0c2ef278ca0cfdc3fe1d96eb6
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-17 12:40:16 +0300

    refactor:change the initialization of bootstrap

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.main.views import comment
+from app.models import Subscriber, User,Comment,Blog
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
@@ -12,6 +13,6 @@ manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,blog = Blog, comment=Comment, subscriber=Subscriber )
 if __name__ == '__main__':
     manager.run()
